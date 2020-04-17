c60d85f29ea6b56e6e96ce90007c49efdccaa310
Eliska Patockova <patockovaeliska@gmail.com>
2020-04-17 09:17:27 -0700

    wrote the movieThis and doThis functions

--- liri.js
+++ liri.js
@@ -59,4 +59,33 @@ function spotifySong(value) {
         }
     })
 }
+function movieThis(value) {
+    if(!value){
+        value = "Pulp Fiction";
+    }
+    axios.get("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy")
+    .then(function(response) {
+            var movieResults = 
+                "--------------------------------------------------------------------" +
+                    "\nMovie Title: " + response.data.Title + 
+                    "\nYear of Release: " + response.data.Year +
+                    "\nIMDB Rating: " + response.data.imdbRating +
+                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                    "\nCountry Produced: " + response.data.Country +
+                    "\nLanguage: " + response.data.Language +
+                    "\nPlot: " + response.data.Plot +
+                    "\nActors/Actresses: " + response.data.Actors;
+            console.log(movieResults);
+    })
+}
 
+function doThis(value) {
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(',');
+        spotifySong(dataArr[0], dataArr[1]);
+    })
+}
